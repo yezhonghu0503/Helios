1eb5646587ffcfa53d094546038ce99dc17196c9
yezhonghu0503 <211736726@qq.com>
2024-04-22 16:40:52 +0800

    feat:Helios

---
+++ .history/src/extension_20240422163511.ts
@@ -0,0 +1,48 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from "vscode";
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand("helios.edit", () => {
+    // 创建一个 WebviewPanel 实例并放置在边栏中
+    const panel = vscode.window.createWebviewPanel(
+      "customSidebar",
+      "Custom Sidebar",
+      vscode.ViewColumn.Beside,
+      {
+        enableScripts: true,
+      }
+    );
+
+    // 读取 HTML 文件的内容并加载到 Webview 中
+    const htmlPath = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test</title>
+</head>
+
+<body>
+    <div>123</div>
+</body>
+
+</html>
+`;
+    panel.webview.html = htmlPath;
+
+    // 处理面板关闭事件
+    panel.onDidDispose(() => {
+      // 在此处清理资源（如果需要）
+    });
+  });
+
+  context.subscriptions.push(disposable);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
