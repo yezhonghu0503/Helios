e5158b049cbbcdbed06b62f3c161b561f22ebf39
yezhonghu0503 <211736726@qq.com>
2024-04-25 15:40:42 +0800

    feat:配置tree view & 构造视图提供器

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,35 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+class DependenciesProvider implements vscode.WebviewViewProvider {
+  context: vscode.ExtensionContext;
+  constructor(context: vscode.ExtensionContext) {
+    this.context = context;
+  }
+  // 实现 resolveWebviewView 方法，用于处理 WebviewView 的创建和设置
+  resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    context: vscode.WebviewViewResolveContext<unknown>,
+    token: vscode.CancellationToken
+  ): void | Thenable<void> {
+    // 配置 WebviewView 的选项
+    webviewView.webview.options = {
+      enableScripts: true,
+      localResourceRoots: [this.context.extensionUri],
+    };
+
+    // 设置 WebviewView 的 HTML 内容，可以在这里指定要加载的网页内容
+    webviewView.webview.html = ``;
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("helios.edit", () => {
-    // 创建一个 WebviewPanel 实例并放置在边栏中
-    const panel = vscode.window.createWebviewPanel(
-      "customSidebar",
-      "Custom Sidebar",
-      vscode.ViewColumn.Beside,
-      {
-        enableScripts: true,
-      }
-    );
+  // vscode.window.registerTreeDataProvider("epubTree", new EpubTreeProvider());
+  vscode.window.registerWebviewViewProvider(
+    "editor",
+    new DependenciesProvider(context)
+  );
 
-    // TODO 如何结合React构建页面
-    const htmlPath = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>test</title>
-</head>
-
-<body>
-    <div>123</div>
-</body>
-
-</html>
-`;
-    panel.webview.html = htmlPath;
-
-    // 处理面板关闭事件
-    panel.onDidDispose(() => {
-      // 在此处清理资源（如果需要）
-    });
-  });
+  let disposable = vscode.commands.registerCommand("helios.edit", () => {});
 
   context.subscriptions.push(disposable);
 }
